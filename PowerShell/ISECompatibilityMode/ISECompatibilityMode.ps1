7e000c93c8da2f01e1c12a0dd0d5365498e7ee42
Oddbjørn Bakke <oddbjorn.bakke@gmail.com>
2014-01-06 15:51:37 +0100

    PSCompatibilityMode

---
+++ PowerShell/ISECompatibilityMode/ISECompatibilityMode.ps1
@@ -0,0 +1,19 @@
+﻿<#
+Tries to force newer version of PowerShell ISE into PowerShell "v2 comaptibility mode".
+You will however loose colors and debugging in this mode.
+#>
+cls
+$psv2mode = $true;
+
+if($psv2mode -and $PSVersionTable.PSVersion.Major -gt 2)
+{
+    Write-Host "CLR: " $PSVersionTable.CLRVersion.Major;
+    Write-Host "PSv: " $PSVersionTable.PSVersion.Major;
+
+    $policy = Get-ExecutionPolicy;
+    powershell.exe -Version 2 -ExecutionPolicy $policy -File $PSCommandPath;
+    exit;
+}
+
+Write-Host "CLR: " $PSVersionTable.CLRVersion.Major;
+Write-Host "PSv: " $PSVersionTable.PSVersion.Major;
